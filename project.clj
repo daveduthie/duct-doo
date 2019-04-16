df29e2bdcf4bc6651d4178c08d86af91e55f66b6
David Duthie <dave.a.duthie@gmail.com>
2019-04-16 17:14:44 +0200

    Upgrade dependencies.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/daveduthie/duct-doo"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[doo "0.1.10"]]
+  :dependencies [[doo "0.1.11"]]
   :src-paths ["src"]
-  :profiles {:dev {:dependencies   [[org.clojure/clojure "1.9.0"]
-                                    [org.clojure/clojurescript "1.10.238"]
-                                    [integrant "0.7.0-alpha2"]]
+  :profiles {:dev {:dependencies   [[org.clojure/clojure "1.10.0"]
+                                    [org.clojure/clojurescript "1.10.520"]
+                                    [integrant "0.7.0"]]
                    :resource-paths ["dev/resources"]
                    :src-paths      ["dev" "test"]}})
